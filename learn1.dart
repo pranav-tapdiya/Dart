5f270bf47454a93cc0afa1b6f44bb527303ad412
PRANAV <120186818+pranav-tapdiya@users.noreply.github.com>
2023-01-10 15:46:17 +0530

    Create learn1.dart

---
+++ learn1.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main(){
+  print('welcome mc');
+   var raman= human();
+   int p;
+   p=20;
+   stdout.write('$p  ');
+   p+=2;
+   double percentage=8.84;
+   print(percentage);
+   print(p);
+   
+}
+class human{
+  var name;
+
+}
