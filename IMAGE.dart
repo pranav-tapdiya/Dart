79d62a4fab5bb641a9b1cb31c904d680adba2d88
PRANAV <120186818+pranav-tapdiya@users.noreply.github.com>
2023-05-21 23:33:03 +0530

    Create IMAGE.dart

---
+++ IMAGE.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Center(child: Text('LEGION',style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.w500,fontFamily: 'Oswald'))),
+    ),
+    body: Container(height: 700,color: Colors.grey,child: Image.asset('assets/images/legion.png',fit: BoxFit.cover,))
+    );
+  }
+}
